5e70baea8e8bb250fe8032cf43255d9bb1511c30
AdarshKhare <>
2016-07-02 13:20:34 -0700

    Cleanup of unused import.

--- src/main/java/com/adarshkhare/spark/sparksample/SparkHelloWorld.java
+++ src/main/java/com/adarshkhare/spark/sparksample/SparkHelloWorld.java
@@ -10,8 +10,6 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
 /**
