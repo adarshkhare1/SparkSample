32efd9ee589bab457a024d72699d084578a40cfa
Adarsh Khare <adkhare@ebay.com>
2016-07-15 14:12:21 -0700

    Refactor and incorporated sample algorithm for multi-class classification.

---
+++ src/main/java/com/adarshkhare/spark/sparksample/MapReduceSample.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.adarshkhare.spark.sparksample;
+
+import java.util.Arrays;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+/**
+ *
+ * @author adkhare
+ */
+public class MapReduceSample
+{
+    public static JavaPairRDD<String, Integer> DoWordCount(JavaSparkContext sc, String inputFile)
+    {
+        // Load our input data.
+        JavaRDD<String> input = sc.textFile(inputFile);
+        JavaRDD<String> words = input.flatMap((String x) -> Arrays.asList(x.split(" ")));
+        // Transform into word and count.
+        JavaPairRDD<String, Integer> counts = words.mapToPair((String x) -> new Tuple2(x, 1));
+        counts.reduceByKey((Integer x, Integer y) -> x + y);
+        return counts;
+    }
+}
--- src/main/java/com/adarshkhare/spark/sparksample/SparkHelloWorld.java
+++ src/main/java/com/adarshkhare/spark/sparksample/SparkHelloWorld.java
@@ -5,14 +5,10 @@
  */
 package com.adarshkhare.spark.sparksample;
 
-import java.util.Arrays;
 import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
-import scala.Tuple2;
 
 /**
  *
@@ -23,30 +19,25 @@ public class SparkHelloWorld
 
     public static void main(String[] args) throws Exception
     {
-
-    
+        SparkConf conf = InitializeSpark();
 
         String inputFile = "c:\\readme.txt";
+        JavaPairRDD<String, Integer> counts = MapReduceSample.DoWordCount(new JavaSparkContext(conf), inputFile);
         String outputFile = "c:\\result.txt";
-
-// Create a Java Spark Context.
-        SparkConf conf = new SparkConf().setAppName("wordCount");
-        conf.setMaster("local");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-
-// Load our input data.
-        JavaRDD<String> input = sc.textFile(inputFile);
-
-// Split up into words.
-        JavaRDD<String> words = input.flatMap((String x) -> Arrays.asList(x.split(" ")));
-
-// Transform into word and count.
-        JavaPairRDD<String, Integer> counts = words.mapToPair((String x) -> new Tuple2(x, 1));
-        counts.reduceByKey((Integer x, Integer y) -> x + y);
-
-// Save the word count back out to a text file, causing evaluation.
         counts.saveAsTextFile(outputFile);
+        
+        MultiClassificationSample.DoMultiClassClassification(new SparkContext(conf));
 
     }
+
+    private static SparkConf InitializeSpark()
+    {
+        // Create a Java Spark Context.
+        SparkConf conf = new SparkConf().setAppName("Samples");
+        conf.setMaster("local");
+        return conf;
+    }
+    
+    
 
 }
